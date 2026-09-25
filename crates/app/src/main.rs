ac7842b94da01aaef8fdcfb85b067e7b0a204446
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2026-09-25 11:10:19 +0200

    bench: revision 3

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let item = bench_lib::parse(r#"{"name":"x","n":2}"#);
+    let item = bench_lib::parse(r#"{"name":"x","n":3}"#);
     let _ = bench_lib::router();
     let _ = bench_lib::reqwest::Client::new();
     println!("{item:?} {}", bench_lib::regex::Regex::new("a+").unwrap().is_match("aa"));
